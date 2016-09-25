17f3b46ae02bffba2796da33a54d90d8a0a330de
TJHL <dont fill this in! />
2016-09-25 16:08:04 -0700

    ljklkj

---
+++ src/level0/yeah.java
@@ -0,0 +1,34 @@
+package level0;
+
+import javax.swing.JOptionPane;
+
+public class yeah {
+public static void main(String[] args) {
+String symbol =	JOptionPane.showInputDialog("what is your zodiac sign?");
+if(symbol.equals("aries")){
+	JOptionPane.showMessageDialog(null,"you will die in 5 years  ");
+}
+	if(symbol.equals("libra")){
+		JOptionPane.showMessageDialog(null,"you will go to jail today");
+	}
+if(symbol.equals("gemini")){
+			JOptionPane.showMessageDialog(null,"you will get sick in the next 5 hours");
+}
+if(symbol.equals("leo")){
+				JOptionPane.showMessageDialog(null,"you will go watch a baseball game today");
+}		
+if(symbol.equals("scorpio")){
+					JOptionPane.showMessageDialog(null,"you will go to arizona today");
+}
+if(symbol.equals("taurus")){	
+						JOptionPane.showMessageDialog(null,"your house will burn down today");
+}	
+if(symbol.equals("virgo")){
+	JOptionPane.showMessageDialog(null,"you will build a robot");
+				
+			
+}		
+}	
+}
+
+
