7ad82a731714893999e9121f3d8c0e6cd4fd8a54
rylr <league@WTS9.attlocal.net>
2016-10-30 16:00:34 -0700

    IO did the number thing
    as;rijtvnasgpdiofas’pdokf’a;sad;aka’s;dlkf’;alksdf’;lkas’dkf

--- src/level0/number.java
+++ src/level0/number.java
@@ -20,8 +20,23 @@ public class number {
 			} else if (n3 < n1 && n3 < n2) {
 				System.out.println(n3 + "," + n1 + "," + n2);
 
-			}
+			//z} else if (n1 < n3 && n1 < n2) {
+				System.out.println(n3 + "," + n2 + "," + n1);
+				if (n1 < n2) {
+					if (n1 < n3) {
+						if (n2 < n3);
+						}else{
+						}
+					}else{       // n1 < n2 ,n1 > n3
+							System.out.format("%d %d %d\n", n1, n2, n3);
+							System.out.format("%d %d %d\n", n1, n3, n2);
+						}
+					}
+				
+					// n2>n1
+
+				
+				}
+	} 
 		}
 	}
-
-}
